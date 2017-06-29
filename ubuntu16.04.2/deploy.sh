d2a4b4615d04cfe3ef6505d8518f696c98799ae1
fugangwang <kevinwangfg@gmail.com>
2017-06-29 19:33:28 -0400

    adding check if 'cm' has been installed (or the virtualenv is activated)

--- ubuntu16.04.2/deploy.sh
+++ ubuntu16.04.2/deploy.sh
@@ -3,6 +3,7 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: deploy.sh <FE_NAME> <FE_USER>"
     exit
 fi
+command -v cm >/dev/null 2>&1 || { echo "'cm comet' not available. Please make sure cloudmesh_client is installed or the virtualenv is activated" >&2; exit 1; }
 hosts=hosts.ini
 echo "[vc_fe]" > $hosts
 echo "$1.sdsc.edu ansible_user=$2" >> $hosts
